97796c82181e58258b80cac59e8d7aed5b1a0fd2
João Pedro Garcia Girotto <godlolpro32@gmail.com>
2025-08-05 17:22:13 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <h1 class="text-4xl font-bold mb-4">Welcome to Your Vue App</h1>
+    <p class="text-lg text-gray-700">This is a simple Vue.js application with Tailwind CSS.</p>
+  </div>
+</template>
